26d06bb299ce6444e4e134de3ce5d0b01040c3ef
helpdesk - pc <contatoluansantos7@gmail.com>
2025-07-10 15:05:07 -0300

    feat: criação do projeto.

---
+++ src/main/java/com/devluan/proVagas/ProVagasApplication.java
@@ -0,0 +1,13 @@
+package com.devluan.proVagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devluan/proVagas/ProVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devluan.proVagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
